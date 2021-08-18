2142e360b80c8fb7182f1da1f7e31524f3d43bf4
Berk <D00225745@student.dkit.ie>
2021-08-18 15:00:48 +0100

    Regex Checker update

--- src/main/java/com/dkit/oopca5/client/RegexChecker.java
+++ src/main/java/com/dkit/oopca5/client/RegexChecker.java
@@ -2,7 +2,58 @@ package com.dkit.oopca5.client;
 
 /* This class should contain static methods to verify input in the application
  */
-
+//https://www.javacodeexamples.com/java-regular-expression-validate-date-example-regex/1504
 public class RegexChecker
 {
+    public static final String caoNumberRegex = "[\\d]{8}";
+    public static final String dobRegex = "\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|[3][01])";
+    public static final String passwordRegex = "[\\S]{8,30}";
+
+
+    public static boolean testCaoNumber(int caoNumber)
+    {
+        boolean result = false;
+        if (Integer.toString(caoNumber).matches(caoNumberRegex))
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public static boolean testDateOfBirth(String dateOfBirth)
+    {
+        boolean result = false;
+        if (dateOfBirth.matches(dobRegex))
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public static boolean testPassword(String password)
+    {
+        boolean result = false;
+        if (password.matches(passwordRegex))
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public static boolean testUserDetails(int caoNumber, String dateOfBirth, String password)
+    {
+        boolean result = false;
+
+        if(testCaoNumber(caoNumber) && testDateOfBirth(dateOfBirth) && testPassword(password))
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
 }
+
