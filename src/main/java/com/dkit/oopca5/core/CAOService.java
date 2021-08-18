7bce5016a2d103d02519cdab91241c1fd09d44d0
Berk <D00225745@student.dkit.ie>
2021-08-18 15:02:03 +0100

    CAO Service update

--- src/main/java/com/dkit/oopca5/core/CAOService.java
+++ src/main/java/com/dkit/oopca5/core/CAOService.java
@@ -10,7 +10,27 @@ public class CAOService
 
     public static final String BREAKING_CHARACTER = "%%";
 
+    //Command strings
+    public static final String QUIT_MENU = "QUIT";
     public static final String REGISTER_COMMAND = "REGISTER";
     public static final String SUCCESSFUL_REGISTER = "REGISTERED";
-    public static final String FAILED_REGISTER = "REG FAILED";
+    public static final String FAILED_REGISTER = "REGISTER FAILED";
+    public static final String LOGIN_COMMAND = "LOGIN";
+    public static final String SUCCESSFUL_LOGIN = "LOGGED IN";
+    public static final String FAILED_LOGIN = "LOGIN FAILED";
+    public static final String LOGOUT = "LOGOUT";
+    public static final String DISPLAY_COURSE = "DISPLAY COURSE";
+    public static final String DISPLAY_ALL_COURSE = "DISPLAY ALL COURSES";
+    public static final String COURSES_EMPTY = "COURSE TABLE EMPTY";
+    public static final String DISPLAY_CURRENT_CHOICE = "DISPLAY CURRENT CHOICE";
+    public static final String UPDATE_CURRENT_CHOICE = "UPDATE CURRENT CHOICE";
+    public static final String UPDATE_CURRENT_CHOICE_SUCCESS = "UPDATE CURRENT CHOICE SUCCESS";
+    public static final String UPDATE_CURRENT_CHOICE_FAILED = "UPDATE CURRENT CHOICE FAILED";
+    public static final String COURSE_NOT_FOUND = "COURSE NOT FOUND";
+
+
+
+    //Response Strings
+    public static final String UNRECOGNISED = "UNKNOWN_COMMAND";
+    public static final String SESSION_TERMINATED = "GOODBYE";
 }
